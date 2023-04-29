bc53a765e34d7b980c600a71c58656fbcf327aee
Asaf <asafkov@gmail.com>
2023-04-29 19:00:46 +0300

    submission version

--- hw1/answers.py
+++ hw1/answers.py
@@ -21,7 +21,7 @@ will obviously preform poorly for most distributions.
 3. True - The test-set role is evaluating how much our model fits the data. Using it in cross validation means training the model over the test-set,
 which should decrease the test-error but at the cost of its reliability in estimating our out-sample error.
 
-4. True - In cross validation, in each iteration we train the model on a different subset of training samples and use the remaining fold to
+4. True - In each iteration of CV we train the model on a different subset of training samples and use the remaining fold to
 estimate the generalization error.   
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -56,7 +56,7 @@ part2_q1 = r"""
 Up to a certain point, increasing k should improve generalization, but the further we stem from that point the more likely we are to underfit the data.
 The point depends on the dataset of course. In general k needs to strike a balance between allowing enough close - "important" neighbors to affect the classification of a point in the space, and also not being large enough that even neighbors that aren't strictly close to the point can affect the classification.
 
-For very large k values (in comparsion to the train-set size), k-NN classification will start to function as a majority-classification where samples are classified as the majority class in the train-set.
+For very large k values (in comparsion to the train-set size), k-NN classification will start to function like a majority-classifier where samples are classified as the majority class in the train-set.
 Generally, very small k values are more likely to overfit (especially if a sample is also its own neighbor), and very large k values lead to underfitting.   
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -71,9 +71,9 @@ An equation: $e^{i\pi} -1 = 0$
 part2_q2 = r"""
 **Your answer:**
 1. The train-set accuracy provides no information whatsoever about how well our model generalizes over the distribution. With the right model-class it can be very easy to get a model with very high train-accuracy, but without a test-set it is impossible to tell if it also suffers from overfitting. 
-For k-NN, the best value of k for training accuracy (if a point is included as its own neighbor) will always be 1, but a model with k=1 will also overfit the data. k-fold CV is better because it does use a validation-set to estimate the generalization.
+For k-NN, the best value of k for training accuracy (if a point is included as its own neighbor) will always be 1, but a model with k=1 will also overfit the data. k-fold CV is better because it uses a validation-set to estimate the generalization.
 
-2. Using the test-set for hyperparameter-tuning will lead to overfitting. K-fold CV is better than this method because the test-set remains completely
+2. Using the test-set for hyperparameter-tuning will overfit the test-set. K-fold CV is better than this method because the test-set remains completely
 seperated from the training-process. 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -112,10 +112,9 @@ part3_q2 = r"""
 For some classes, there are some clear patterns in the weights image that numbers in the class usually exhibit. So, the model develop these patterns over the course of the training process and 
 then a prediction is made over a sample based on which patterns fit it best.
 
-2. kNN model classifies based on distance in the feature-space, so the assumption is that there a distance-based pattern the should yield good classification ("close" samples are similar).
-Our interpertation of the lineaer classifier model also relies on similiarty in geometrical patterns which can also be intereperted as a measure of how close a sample is to samples in a given class.
-There is still quite a difference though, because kNN relies on non-learned decision boundries, while a lineaer classifier relies on
-learned hyperplanes to seperate the classes in the feature space.
+2. kNN model classifies based on distance in the feature-space, so the assumption is that there a distance-based pattern the should yield good classification ("close" samples are more likely to be of the same class).
+Our interpertation of the lineaer classifier model also relies on similiarty in geometrical patterns which can also be intereperted as a measure of how close it is to a "representer" of a given class.
+The models are different in the senes that kNN relies on non-learned decision boundries, while a lineaer classifier relies on learned hyperplanes to seperate the classes in the feature space.
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
@@ -151,7 +150,7 @@ part4_q1 = r"""
 Ideally the residuals should display a random scatter tightly around 0, which indicates that predication error
 is small across the entire set. It should also be linear, and the pattern should remain similar (uniform) across the $\hat{y}$ axis.
 In the plot of the 5-best features, the residuals aren't as tightly spread around 0 as they are in out final model, and it also displays
-a curved pattern which indicates a linear regressor hypotheses class doesn't quite fit as a model (when limiting it to only those 5 features).
+a curved pattern which indicates a linear regressor hypotheses class isn't well suited for the learning problem (when limiting it to only those 5 features).
 
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -166,7 +165,7 @@ An equation: $e^{i\pi} -1 = 0$
 part4_q2 = r"""
 **Your answer:**
 1. It remains a linear regression model, because the predictions are stil being made using a linear function:
-$y_{pred} = W^TX+b$
+$y_{pred} = W^TX+b$ (in this case $X$ is the modified feature matrix, but that's irrelevent to the prediction rule).
 2. Yes, for any non-linear function of the features, we can apply a mapping that will fit the function with the a suitable weight
 matrix that will serve to apply coefficents. 
 3. It will still be a hyperplane, just in a different space, and the decision boundary will depend on the applied mapping.
@@ -183,8 +182,8 @@ An equation: $e^{i\pi} -1 = 0$
 part4_q3 = r"""
 **Your answer:**
 1. logspace allows us to effeciently scour for an estimation for the best $\lambda$ over values with different orders of magnitude.
-Achieving the same goal with linespace will take more time, and when we found an estimation for the order of mantiude suitable for $\lambda$
-We can cross validate over a linespace defined range to scour around it.
+Achieving the same goal with linespace could potentially take much longer. When we've found an estimation for the order of mantiude suitable for $\lambda$
+we can cross validate over a linespace defined range to scour around it.
 2. We had two hyperparameters - degree with x=3 possible values, and lambda with y=20 possible values. We applied gridsearch with k=3.
 The number of times we fit the data is therefore: $kxy = 3*3*20=180$
 
